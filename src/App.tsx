1373814468f622248c68ecec1c78ee31a8592621
David <Rigby335@hotmail.com>
2023-12-05 10:01:46 -0800

    feat: dynamic array of 24 objects with id properties, one for each door

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import Door from "./components/Door";
 
 function App() {
-  return <></>;
+  const [doorArray, setDoorArray] = useState<object[]>([]);
+
+  useEffect(() => {
+    const generateDoors = () => {
+      const myArr = [];
+      for (let i = 1; i <= 24; i++) {
+        const doorObject = {
+          id: i,
+        };
+
+        myArr.push(doorObject);
+      }
+      setDoorArray(myArr);
+    };
+    generateDoors();
+  }, []);
+
+  console.log(doorArray);
+
+  return (
+    <>
+      <Door />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Door.tsx
@@ -0,0 +1,3 @@
+export default function Door() {
+  return <div>Door</div>;
+}
